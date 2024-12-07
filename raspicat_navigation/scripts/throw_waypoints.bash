62482fa26783e9d9f2d933044f1a6f866c67202e
makotoyoshigoe <myoshigo0127@gmail.com>
2024-12-07 23:40:12 +0900

    Update

--- raspicat_navigation/scripts/throw_waypoints.bash
+++ raspicat_navigation/scripts/throw_waypoints.bash
@@ -3,11 +3,13 @@
 # sudo wget https://github.com/mikefarah/yq/releases/download/v4.40.1/yq_linux_amd64 -O /usr/local/bin/yq
 # sudo chmod +x /usr/local/bin/yq
 
-# yaml_file="$HOME/src/raspicat_slam_navigation/raspicat_navigation/config/waypoint/" # 追加分のwaypointのyamlのパス
-yaml_file="$HOME/waypoint/wp_kakuninn.yaml" 
+yaml_file="$HOME/mugimaru-docker/ros2_ws/src/raspicat_slam_navigation/raspicat_navigation/config/waypoint/wp_kakuninn.yaml" # 追加分のwaypointのyamlのパス
+#yaml_file="$HOME/waypoint/wp_kakuninn.yaml" 
 
 # Read YAML file and extract waypoints using yq tool (assuming it's installed)
 waypoints=($(yq eval '.waypoints | keys | .[]' "$yaml_file"))
+
+pose_array="{"
 
 for waypoint in "${waypoints[@]}"; do
     pose_x=$(yq eval ".waypoints.$waypoint.pose[0]" "$yaml_file")
@@ -31,10 +33,14 @@ for waypoint in "${waypoints[@]}"; do
         orientation: { x: $orientation_x, y: $orientation_y, w: $orientation_w, z: $orientation_z}" &
 #COMMENTOUT
 
-    # debug
-    echo "pose x: $pose_x, y: $pose_y, z: $pose_z"
-    echo "orientation  x: $orientation_x, y: $orienation_y, w: $orientation_w, z: $orientation_z"
 
+    # debug
+    #echo "pose x: $pose_x, y: $pose_y, z: $pose_z"
+    #echo "orientation  x: $orientation_x, y: $orienation_y, w: $orientation_w, z: $orientation_z"
     # Wait for Enter key press to proceed to the next waypoint
-    read -p "Press Enter to proceed to the next waypoint: "
+    #read -p "Press Enter to proceed to the next waypoint: "
+	wait
 done
+
+#ros2 action send_goal /follow_waypoints nav2_msgs/action/FollowWaypoints pose_array
+echo $pose_array
